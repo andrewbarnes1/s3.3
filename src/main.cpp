4d293f4bda7d9a65f32c4af8edbbe9cfdbdbaba2
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2017-12-05 14:13:48 +0000

    tone output

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
-#include <mbed.h>
+#include "mbed.h"
 
-int main() {
+DigitalIn fire(D4);
+PwmOut spkr(D6);
 
-    // put your setup code here, to run once:
-
-    while(1) {
-        // put your main code here, to run repeatedly:
+int main()
+{
+    while (1) {
+        for (float i=2000.0; i<10000.0; i+=100) {
+            spkr.period(1.0/i);
+            spkr=0.5;
+            wait(0.02);
+        }
+        spkr=0.0;
+        while(!fire);
     }
 }
