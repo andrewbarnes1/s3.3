46f822e0c1182783c4b172f39947835ee66d9e40
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2018-01-08 07:55:14 +0000

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,22 @@
 #include "mbed.h"
 
 DigitalIn fire(D4);
-PwmOut spkr(D6);
+DigitalOut green(D9,1);
+
+PwmOut speaker(D6);
 
 int main()
 {
     while (1) {
-        for (float i=2000.0; i<10000.0; i+=100) {
-            spkr.period(1.0/i);
-            spkr=0.5;
-            wait(0.02);
+        float f;
+        green.write(1);
+        for (f=20.0; f<20.0e3; f+=100) {
+            speaker.period(1.0/f);
+            speaker.write(0.5);
+            wait(0.1);
         }
-        spkr=0.0;
+        speaker.write(0.0);
+        green.write(0);
         while(!fire);
     }
 }
