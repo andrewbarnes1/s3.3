6bee19a3c3d9051c32815801ead2335eee1e4ac4
Student <student@C17775444-lnx.offcampusnetwork.co.uk>
2018-01-29 18:00:14 +0000

    Added notes C D and E to the speaker.

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,38 @@ PwmOut speaker(D6);
 
 int main()
 {
-    while (1) {
-        float f;
-        green.write(1);
-        for (f=20.0; f<20.0e3; f+=100) {
-            speaker.period(1.0/f);
-            speaker.write(0.5);
-            wait(0.1);
-        }
-        speaker.write(0.0);
-        green.write(0);
-        while(!fire);
+  while (1) {
+    float f;
+    green.write(1);
+
+    for(int i = 0; i < 25; i++) {
+      speaker.period(1.0/261);
+      speaker.write(0.5);
+      wait(0.1);
     }
+    for(int i = 0; i < 25; i++) {
+      speaker.period(1.0/293);
+      speaker.write(0.5);
+      wait(0.1);
+    }
+    for(int i = 0; i < 25; i++) {
+      speaker.period(1.0/329);
+      speaker.write(0.5);
+      wait(0.1);
+    }
+
+    /*
+    // 20hz ; 20khz ; Incrementation
+    for (f=20.00; f<5.0e3; f+=300) {
+    speaker.period(1.0/f);
+    speaker.write(0.5);
+    wait(0.1);
+  }
+
+  speaker.write(0.0);
+  green.write(0);
+  while(!fire);
+  */
+
+}
 }
